1d07e25955418076d68d17f53956e8594547e793
kartik_989 <kartikjangu@gmail.com>
2023-08-26 23:56:36 +0530

    lin

--- DSA/Recursion/programs/doublylinklist.cpp
+++ DSA/Recursion/programs/doublylinklist.cpp
@@ -1,6 +1,38 @@
 #include<iostream>
 using namespace std;
 
+class Node{
+    public:
+    int data;
+    Node * prev;
+    Node * next; 
+
+
+    public:
+    Node(int a){
+        data=a;
+        prev=NULL;
+        next=NULL;
+    }
+};
+
+Node * HEAD=NULL;
+
+void Insertathead(int x){
+
+    Node * p = new Node(x);
+    
+    if(HEAD==NULL){
+        HEAD= p;
+        return ;
+    }
+
+    HEAD->prev=p;
+    p->next=HEAD;
+    HEAD=p;
+
+}
+
 int main(){
 
 }
