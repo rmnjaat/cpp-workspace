1394c03a30ea2ecec38b48997d96d8f084099621
rmnjaat <ramanjangu01@outlook.com>
2023-10-27 00:32:28 +0530

    adding OS project

--- OS_CA_3.cpp
+++ OS_CA_3.cpp
@@ -1,42 +1,57 @@
-#include<stdio.h>
-#include<string.h>
 
-struct student
-{
-    
-    char name[20];
-    int no_gift;
 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+// Define the structure
+struct student {
+    char name[50];
+    int gift_number;
 };
 
-
-int main(){
-
-    struct student arr[10];
-    
-    for(int i=0;i<10;i++){
-        char c[20];
-        int g;
-        
-        printf("Enter %d \n",i);
-        // fflush(stdin);
-        scanf("%s",c);
-        scanf("%d",&g);
-        fflush(stdin);
-
-
-        strcpy(arr[i].name,c);
-
-        arr[i].no_gift=g;
-
-
-    }
-
-
-    for(int i=0;i<10;i++){
-        printf("%s : %d \n",arr[i].name,arr[i].no_gift);
-    }
-
+// Comparison function for sorting
+int compareByGiftNumber(const void *a, const void *b) {
+    return ((struct student*)a)->gift_number - ((struct student*)b)->gift_number;
 }
 
+// Function to sort an array of stu_arr by gift number
+void sortstu_arrByGiftNumber(struct student *stu_arr, int num_student) {
+    qsort(stu_arr, num_student, sizeof(struct student), compareByGiftNumber);
+}
 
+int main() {
+    // Define an array of stu_arr
+    int num_student=3;
+    struct student stu_arr[num_student];
+
+      for(int i=0;i<num_student;i++){
+        char c[4];
+        int g;
+        
+        printf("Enter name of %dst student :\n",i);
+        // fflush(stdin);
+        scanf("%s",c);
+
+        printf("Enter Number of gift \n");
+        scanf("%d",&g);
+        // fflush(stdin);
+
+        strcpy(stu_arr[i].name,c);
+
+        stu_arr[i].gift_number=g;
+
+    }
+
+
+    // Sort the array based on "gift_number"
+    sortstu_arrByGiftNumber(stu_arr, num_student);
+
+    // Print the sorted list of stu_arr
+    for (int i = num_student-1; i >=0; i--) {
+        
+        printf("Name: %s, Gift Number: %d\n", stu_arr[i].name, stu_arr[i].gift_number);
+    }
+
+    return 0;
+}
