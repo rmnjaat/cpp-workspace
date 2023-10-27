c8156c868be14235f5e69a4c410801a7ce61cd42
rmnjaat <ramanjangu01@outlook.com>
2023-10-27 16:53:30 +0530

    ca updated

--- OS_CA_3.cpp
+++ OS_CA_3.cpp
@@ -10,12 +10,15 @@ struct student {
     int gift_number;
 };
 
-// Comparison function for sorting
+
 int compareByGiftNumber(const void *a, const void *b) {
-    return ((struct student*)a)->gift_number - ((struct student*)b)->gift_number;
+   struct student *aa= (struct student*)a; 
+   struct student *bb = (struct student*)b; 
+
+   return  bb->gift_number - aa->gift_number ;
 }
 
-// Function to sort an array of stu_arr by gift number
+// Fuction to sort an array of stu_arr by gift number
 void sortstu_arrByGiftNumber(struct student *stu_arr, int num_student) {
     qsort(stu_arr, num_student, sizeof(struct student), compareByGiftNumber);
 }
@@ -29,7 +32,7 @@ int main() {
         char c[4];
         int g;
         
-        printf("Enter name of %dst student :\n",i);
+        printf("Enter name of %dst student :\n",i+1);
         // fflush(stdin);
         scanf("%s",c);
 
@@ -47,8 +50,10 @@ int main() {
     // Sort the array based on "gift_number"
     sortstu_arrByGiftNumber(stu_arr, num_student);
 
+
+    printf("Printing result according to their priority \n");
     // Print the sorted list of stu_arr
-    for (int i = num_student-1; i >=0; i--) {
+    for (int i = 0; i <num_student; i++) {
         
         printf("Name: %s, Gift Number: %d\n", stu_arr[i].name, stu_arr[i].gift_number);
     }
