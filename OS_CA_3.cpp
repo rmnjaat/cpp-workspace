a80effefaf2ee9f69fb49098f763405e6e30b9e0
rmnjaat <ramanjangu01@outlook.com>
2023-10-28 14:46:43 +0530

    final project

--- OS_CA_3.cpp
+++ OS_CA_3.cpp
@@ -1,61 +1,50 @@
 
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <time.h>
 
 // Define the structure
-struct student {
-    char name[50];
+struct student
+{
+    // char name[50];
     int gift_number;
 };
 
+int compareByGiftNumber(const void *a, const void *b)
+{
+    struct student *aa = (struct student *)a;
+    struct student *bb = (struct student *)b;
 
-int compareByGiftNumber(const void *a, const void *b) {
-   struct student *aa= (struct student*)a; 
-   struct student *bb = (struct student*)b; 
-
-   return  bb->gift_number - aa->gift_number ;
+    return bb->gift_number - aa->gift_number;
 }
 
 // Fuction to sort an array of stu_arr by gift number
-void sortstu_arrByGiftNumber(struct student *stu_arr, int num_student) {
+void sortstu_arrByGiftNumber(struct student *stu_arr, int num_student)
+{
     qsort(stu_arr, num_student, sizeof(struct student), compareByGiftNumber);
 }
 
-int main() {
+int main()
+{
     // Define an array of stu_arr
-    int num_student=3;
+    int num_student = 3;
     struct student stu_arr[num_student];
-
-      for(int i=0;i<num_student;i++){
-        char c[4];
-        int g;
-        
-        printf("Enter name of %dst student :\n",i+1);
-        // fflush(stdin);
-        scanf("%s",c);
-
-        printf("Enter Number of gift \n");
-        scanf("%d",&g);
-        // fflush(stdin);
-
-        strcpy(stu_arr[i].name,c);
-
-        stu_arr[i].gift_number=g;
-
+    srand(time(NULL));
+    for (int i = 0; i < num_student; i++)
+    {
+        stu_arr[i].gift_number = rand() % 100;
     }
-
 
     // Sort the array based on "gift_number"
     sortstu_arrByGiftNumber(stu_arr, num_student);
 
-
     printf("Printing result according to their priority \n");
     // Print the sorted list of stu_arr
-    for (int i = 0; i <num_student; i++) {
-        
-        printf("Name: %s, Gift Number: %d\n", stu_arr[i].name, stu_arr[i].gift_number);
+    for (int i = 0; i < num_student; i++)
+    {
+
+        printf("Name: P%d , Gift Number: %d\n", i + 1, stu_arr[i].gift_number);
     }
 
     return 0;
