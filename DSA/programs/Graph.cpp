5d65c75966414fd07925c5273c313b0613a3fdad
rmnjaat <ramanjangu01@outlook.com>
2023-11-18 14:07:05 +0530

    Optmised

---
+++ DSA/programs/Graph.cpp
@@ -0,0 +1,136 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+ //dfs
+void print(int ** edges ,int n,int sv,bool * visited){
+    cout<<sv<<endl;
+    visited[sv]=true;
+    for(int i=0;i<n;i++){
+        if(i==sv){
+            continue;  
+        }
+        if(edges[sv][i]==1){
+            if(visited[i]){
+                continue;
+            }
+            print(edges,n,i,visited);
+        }
+    }
+    
+}
+
+
+//bfs
+
+
+void bfs(int ** edges,int n,int sv,bool visited[]){
+    queue<int> pendingVerticies;
+    pendingVerticies.push(sv);
+    visited[sv]=true;
+    while(!pendingVerticies.empty()){
+        int curr=pendingVerticies.front();
+        cout<<curr<<endl;
+        for(int i=0;i<n;i++){
+
+            if(i==curr) continue;
+
+            if(edges[curr][i] && ! visited[i]){
+                pendingVerticies.push(i);
+                visited[i]=true;
+            }
+        }
+        pendingVerticies.pop();
+    }
+
+
+    
+
+
+}
+
+// for disconnected 
+void BFS(int ** edges,int n){
+     bool * visited = new bool[n];
+    for(int i=0;i<n;i++){
+        visited[i]=false;
+    }
+
+    for(int i=0;i<n;i++){
+        if(!visited[i]){
+            bfs(edges,n,i,visited);
+        }
+    }
+
+    delete [] visited;
+
+}
+//for disconnected
+void DFS(int ** edges ,int n){
+    bool * visited = new bool[n];
+    for(int i=0;i<n;i++){
+        visited[i]=false;
+    }
+
+    for(int i=0;i<n;i++){
+        if(!visited[i]){
+            print(edges,n,i,visited);
+        }
+    }
+
+    delete [] visited;
+
+}
+
+//hash path
+
+
+int main(){
+
+    int n;
+    int e;
+    cin>>n>>e;
+    int ** edges=new int*[n];
+
+    for(int i=0;i<n;i++){
+        edges[i]= new int[n];
+        for(int j=0;j<n;j++){
+            edges[i][j]=0;
+        }
+    }
+
+    for(int i=0;i<e;i++){
+        int f,s;
+        cin>>f>>s;
+
+        edges[f][s]=1;
+        edges[s][f]=1;
+    }
+
+    bool * visited = new bool [n];
+    for(int i=0;i<n;i++){
+        visited[i]=0;
+    }
+
+    // print(edges,n,0,visited);
+
+   
+
+    BFS(edges,n);
+
+    cout<<"--------------------"<<endl;
+    DFS(edges,n);
+
+
+
+
+
+
+
+    delete [] visited;
+    for(int i=0;i<n;i++){
+        delete [] edges[i];
+    }
+
+
+
+}
--- DSA/programs/sortingalgo.cpp
+++ DSA/programs/sortingalgo.cpp
@@ -1,74 +1,44 @@
 
-// #include <iostream>
-// using namespace std;
-
-// void insertion_sort(int arr[], int n)
-// {
-//     for (int i = 1; i < n; i++)
-//     {
-//         int temp = arr[i];
-//         int j = i - 1;
-
-//         while (j >= 0 && arr[j] > temp)
-//         {
-//             arr[j + 1] = arr[j];
-//             j--;
-//         }
-
-//         arr[j + 1] = temp;
-//     }
-// }
-
-// int main()
-// {
-//     int a[] = {1, 8, 6, 7, 23};
-
-//     insertion_sort(a, 5);
-//     for (auto val : a)
-//     {
-//         cout << val << " ";
-//     }
-// }
-
-// #include <iostream>
-// #include <bits/stdc++.h>
-// using namespace std;
-
-// void selsort(int arr[], int n)
-// {
-
-//     for (int i = 0; i < n; i++)
-//     {
-//         int min = i;
-//         for (int j = i + 1; j < n; j++)
-//         {
-//             if (arr[j] < arr[i])
-//             {
-//                 min = j;
-//             }
-//         }
-//         if (min != i)
-//         {
-//             swap(arr[min], arr[i]);
-//         }
-//     }
-// }
-
-// int main()
-// {
-//     int arr[] = {1, 8, 4, 6, 3};
-
-//     selsort(arr, 5);
-
-//     for (auto v : arr)
-//     {
-//         cout << v << " ";
-//     }
-// }
-
 #include <iostream>
-#include<bits/stdc++.h>
 using namespace std;
+
+void insertion_sort(int arr[], int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int key = arr[i];
+        int j=i-1;
+
+            while(j>=0  && arr[j] > key ){
+                arr[j+1]=arr[j];
+                j--;
+            }
+
+            arr[j+1]=key;
+
+    }
+}
+
+
+void selsort(int arr[], int n)
+{
+
+    for (int i = 0; i < n; i++)
+   {
+        int min_index=i;
+
+        for (int j = min_index+1; j < n; j++)
+        {
+            if(arr[j] < arr[min_index]){
+                min_index=j;
+            }
+        }
+
+        swap(arr[i],arr[min_index]);
+        
+    }
+}
+
 
 void bubblesort(int arr[], int n)
 {
@@ -84,12 +54,17 @@ void bubblesort(int arr[], int n)
 
 int main()
 {
-    int arr[] = {1, 9, 3, 6, 2};
+    int a[] = {1, 8, 6, 7, 23};
 
-    bubblesort(arr, 5);
-
-    for (auto v : arr)
+    // insertion_sort(a, 5);
+    selsort(a,5);
+    for (auto val : a)
     {
-        cout << v << " ";
+        cout << val << " ";
     }
 }
+
+
+
+
+
